8adf87e741dd824c2eb9ffda33f9c7946816ed78
Prashik Wankhade <111126326+PrashikWankhadeSCOE@users.noreply.github.com>
2023-12-27 22:53:11 +0530

    Stack Problem Statement

--- DSA/Stack/StackProblem/Program1.java
+++ DSA/Stack/StackProblem/Program1.java
@@ -1,8 +1,32 @@
-class RevString{
+import java.util.*;
 
+class RevString{
+	
+	String reverseString(String str){
+	
+		Stack<Character> s = new Stack<Character>();
+	
+		for(int top = 0;top<str.length();top++){
+			s.push(str.charAt(top));
+		}
+	
+		String ret = "";
+	
+		while(!s.empty()){
+			ret = ret + s.pop();
+		}
+	
+		return ret;
+	}
 }
 class Client{
 	public static void main(String [] args){
-	
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+
+		RevString obj = new RevString();
+
+		System.out.println(obj.reverseString(str));
+
 	}
 }
